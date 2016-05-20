eb17c373d74048d70cb1d7e01deee520fb5a6c50
xnxs <xnxs@xnxs-PC>
2016-05-20 18:53:39 +0800

    5

---
+++ zznt/src/main/java/com/my/release03/StringManager.java
@@ -0,0 +1,44 @@
+package com.my.release03;
+
+import java.util.Hashtable;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+public class StringManager {
+	
+	private ResourceBundle bundle;
+	
+	private StringManager(String packageName) {
+		String bundleName = packageName + ".LocalStrings";
+		bundle = ResourceBundle.getBundle(bundleName);
+	}
+	
+	public String getString(String key) {
+		if (key == null) {
+			String msg = "key is null";
+			
+			throw new NullPointerException(msg);
+		}
+		
+		String str = null;
+		
+		try {
+		    str = bundle.getString(key);
+        } catch (MissingResourceException mre) {
+            str = "Cannot find message associated with key '" + key + "'";
+        }
+		
+		return str;
+	}
+	
+	private static Hashtable<String, StringManager> managers = new Hashtable<String, StringManager>();
+	
+	public synchronized static StringManager getManager(String packageName) {
+		StringManager mgr = (StringManager)managers.get(packageName);
+		if (mgr == null) {
+		    mgr = new StringManager(packageName);
+		    managers.put(packageName, mgr);
+		}
+		return mgr;
+    }
+}
